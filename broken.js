4edc25cd0489c8cb7fef1a0c441c7b1278682407
黄玄 <huxpro@gmail.com>
2016-10-02 16:05:36 +0800

    init

---
+++ broken.js
@@ -0,0 +1,6 @@
+document.write(
+	"Im the script of broken page" +
+	"<br/>" +
+	"I thought the window.Lib() should be true, and the real value is: " + Lib() 
+)
+
---
+++ lib-cached.js
@@ -0,0 +1,13 @@
+window.Lib = function(){
+	return true
+}
+
+console.log(document.body.innerHTML)
+document.body.innerHTML += (
+	"<p>" +
+	"Im a JS library and Im cached with 10min." +
+	"<br/>" +
+	"I put a function Lib into Global which has value: " + Lib() +
+	"</p>"
+)
+
